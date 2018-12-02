00ffbee49ce9416c0328af3b5e3401da2637253b
ARATA <newbluelight1031@gmail.com>
2018-12-02 18:04:21 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   pass = ENV['LOGIN_PASSWORD']
   
 　config.action_mailer.delivery_method = :smtp
-　config.action_mailer.raise_delivery_errors = true
+　#config.action_mailer.raise_delivery_errors = true
 　#host = 'http://quiet-retreat-78843.herokuapp.com'
 　#config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {

1521fe5c6bac940c81b865100799cecf75fdba06
ARATA <newbluelight1031@gmail.com>
2018-12-02 02:24:57 +0000

    change production env for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
-  ENV['LOGIN_NAME']
-  ENV['LOGIN_PASSWORD']
+ config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+	address:               'smtp.gmail.com',
+	port:      	           587,
+	domain:                'example.com',
+	user_name:             ENV['LOGIN_NAME'],
+	password:              ENV['LOGIN_PASSWORD'],
+	authentication:        'plain',
+	enable_starttls_auto:  true
+}
   
 end
